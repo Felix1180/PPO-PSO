55fcc9a36ab5c6851bacee307c224e4897702600
Felix1180 <134295039+Felix1180@users.noreply.github.com>
2023-11-28 18:18:06 +0800

    Memberi komentar

--- nomor1B.py
+++ nomor1B.py
@@ -4,93 +4,128 @@ import matplotlib.pyplot as plt
 from prettytable import PrettyTable
 
 def objective_function(position):
+    #Menghitung nilai fungsi objektif untuk suatu fungsi
     x = position[0]
     return x ** 2 - 10 * math.sin(2 * math.pi * x)
 
 class Particle:
     def __init__(self, dimensi, initial_position):
+        #Konstruktor kelas Particle untuk pembuatan partikel
         self.position = initial_position[:]
+        #Membuat salinan independen dari posisi awal dan disetor ke position
         self.velocity = [0.0] * dimensi
+        #Inisialisasi v awal partikel
         self.pbest = self.position[:]
+        #Membuat salinan independen dari position untuk pbest awal
         self.pbest_value = objective_function(self.position)
+        #pbest value awal dari partikel adalah obj func(position)
 
 def update_velocity(particle, gbest, w, c1=2, c2=2):
+    #Fungsi untuk mengupdadte kececpatan partikel.
+
     for i in range(len(particle.velocity)):
         cognitive = c1 * random.uniform(0, 1) * (particle.pbest[i] - particle.position[i])
+        #Kecenderungan bergerak mengikuti pbest
         social = c2 * random.uniform(0, 1) * (gbest[i] - particle.position[i])
+        #Kecenderungan bergerak mengikuti gbest
         old_velocity = particle.velocity[i]
+        #Mengambil kecepatan sekarang
         new_velocity = w * old_velocity + cognitive + social
+        #Kecepatan baru
         particle.velocity[i] = new_velocity
+        #Update kecepatan partikel dengan kecepatan baru
 
 def update_position(particle):
+    #Fungsi untuk mengupdate posisi partikel.
     for i in range(len(particle.position)):
         new_position = particle.position[i] + particle.velocity[i]
+        #Posisi baru = posisi partikel + kecepatan partikel
         particle.position[i] = max(-5.2, min(5.2, new_position))
+        #Menerapkan batas posisi partikel untuk tetap berada pada rentang soal.
 
 def generate_random_positions(dimensi, jumlah_posisi):
+    #Fungsi untuk membuat angka random sebagai posisi awal partikel dengan batas (-5.2,5.2)
     return [[random.uniform(-5.2, 5.2) for _ in range(dimensi)] for _ in range(jumlah_posisi)]
 
 def pso(dimensi, jumlah_partikel, jumlah_iterasi, initial_particles=None):
+    #Algoritma Particle Swarm Optimization (PSO)
     particles = initial_particles or [Particle(dimensi, [0.0] * dimensi) for _ in range(jumlah_partikel)]
+    """Jika initial particle tidak diinisialisasi (none), maka menjalankan list comprehension itu
+    untuk membuat particle dengan nilai [0.0] tapi kalau tidak kosong ya ikuti isinya itu"""
+
     gbest = min(particles, key=lambda p: p.pbest_value).pbest[:]
+    """gbest diambil dari nilai terkecil menggunakan fungsi (min) 
+    dari atribut pbest_value terkecil dari tiap-tiap partikel
+    lalu salin pbest[:]-nya tersebut menjadi nilai gbest"""
 
     table = PrettyTable()
+    #Pembuatan Pretty Table dan Kolomnya apa saja
     table.field_names = ["Iterasi", "Posisi (x)", "f(x)", "pBest", "gBest", "v"]
 
-    # Lists to store data for visualization
+    # List penyimpan data untuk visualisasi
     iteration_list = []
     position_list = []
 
     for iteration in range(jumlah_iterasi):
-        iteration_list.append(iteration + 1)
+        iteration_list.append(iteration + 1)  #Tambahkan info ke list tadi
         current_positions = [particle.position[0] for particle in particles]
-        position_list.append(current_positions)
+        position_list.append(current_positions) #Tambahkan info posisi ini ke list
+
+        #Buat ngisi kolom
         fx_values = [objective_function(particle.position) for particle in particles]
 
         w = 0.9 - (iteration / jumlah_iterasi) * 0.4  # Menurunkan inertia weight tiap iterasi
 
+        #Evaluasi fungsi objektif
         for particle in particles:
             current_fitness = objective_function(particle.position)
 
+            """Jika fitness sekarang lebih kecil dari nilai pbest,
+            maka pbest dari partikel adalah posisi partikel sekarang 
+            dan valuenya adalah fitness terkini"""
             if current_fitness < particle.pbest_value:
                 particle.pbest = particle.position[:]
                 particle.pbest_value = current_fitness
 
+            """Kemudian jika current fitness juga lebih kecil
+            dari pada nilai objektif function dari gbest
+            maka si gbest nya diubah menjadi posisi partikel itu"""
             if current_fitness < objective_function(gbest):
                 gbest = particle.position[:]
 
-        for particle in particles:
+        for particle in particles: #Update dulu posisi partikelnya
             update_velocity(particle, gbest, w)
 
         row = [iteration + 1,
-               [round(pos, 4) for pos in current_positions],
-               [round(fx, 4) for fx in fx_values],
-               [round(pbest, 4) for pbest in [particle.pbest[0] for particle in particles]],
-               [round(value, 4) for value in gbest],
-               [round(velocity, 5) for velocity in [particle.velocity[0] for particle in particles]]
-               ]
+               [round(pos, 4) for pos in current_positions], #x
+               [round(fx, 4) for fx in fx_values], #f(x)
+               [round(pbest, 4) for pbest in [particle.pbest[0] for particle in particles]],#pbest
+               [round(value, 4) for value in gbest], #gbest
+               [round(velocity, 5) for velocity in [particle.velocity[0] for particle in particles]]#v
+               ] #Menambahkan isi barisnya apa aja
 
-        table.add_row(row)
+        table.add_row(row) #barisnya ditambahin ke table
 
-        for particle in particles:
+
+        for particle in particles: #Setelah itu update posisi
             update_position(particle)
 
-    # Print the table
+    # Print table kalo udah selesai iterasinya
     print(table)
 
-    # Plot objective function and particle positions in the same figure
+    #Plot Objective Function dan Posisi Partikel
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
 
-    # Plot objective function on the left subplot (ax1)
-    x_values = [i / 100 for i in range(-520, 521)]
-    y_values = [objective_function([x]) for x in x_values]
+    # Plot objective function di kiri
+    x_values = [i / 100 for i in range(-520, 521)] #Sumbu X
+    y_values = [objective_function([x]) for x in x_values] #Sumbu Y
     ax1.plot(x_values, y_values, label='f(x)', color='blue')
     ax1.set_xlabel('x')
     ax1.set_ylabel('f(x)', color='blue')
     ax1.tick_params(axis='y', labelcolor='blue')
     ax1.legend(loc='upper left')
 
-    # Plot particle positions on the right subplot (ax2)
+    # Plot posisi partikel positions dikanan
     for i in range(jumlah_partikel):
         ax2.plot(iteration_list, [positions[i] for positions in position_list], label=f'Particle {i + 1}', marker='o')
 
@@ -104,12 +139,14 @@ def pso(dimensi, jumlah_partikel, jumlah_iterasi, initial_particles=None):
     return gbest, objective_function(gbest)
 
 if __name__ == "__main__":
-    dimensi = 1
-    jumlah_partikel = 3
-    jumlah_iterasi = 300
+    dimensi = 1 #Mau berapa dimensi ganti sesuai keinginan
+    jumlah_partikel = 3 #Mau berapa partikel
+    jumlah_iterasi = 300 #Mau berapa iterasi
 
-    initial_positions = generate_random_positions(dimensi, 3)
+    initial_positions = generate_random_positions(dimensi, 3) #Posisi awal partikel random
+    #Bikin objek partikel terus di tampung ke particles
     particles = [Particle(dimensi, initial_position) for initial_position in initial_positions]
 
+    #hasil dan nilai optimum nanti keisi dengan gbest dan obj func (gbest) sesuai dengan apa yang di return def pso
     hasil, nilai_optimum = pso(dimensi, jumlah_partikel, jumlah_iterasi, initial_particles=particles)
     print(f"Hasil optimasi: gBest={hasil}, f(x)={nilai_optimum}")
--- nomor2B.py
+++ nomor2B.py
@@ -15,38 +15,26 @@ class Particle:
         self.pbest_value = objective_function(self.position)
 
 def update_velocity(particle, gbest, w, c1=2, c2=2):
-    # Fungsi untuk mengupdate kecepatan partikel.
-
     for i in range(len(particle.velocity)):
-        cognitive = c1 * random.uniform(0, 1) * (particle.pbest[i] - particle.position[i])  # Kecenderungan bergerak mengikuti pbest
-        social = c2 * random.uniform(0, 1) * (gbest[i] - particle.position[i])  # Kecenderungan bergerak mengikuti Gbest
-        # Simpan nilai v sebelum diperbarui
+        cognitive = c1 * random.uniform(0, 1) * (particle.pbest[i] - particle.position[i])
+        social = c2 * random.uniform(0, 1) * (gbest[i] - particle.position[i])
         old_velocity = particle.velocity[i]
-        new_velocity = w * old_velocity + cognitive + social  # Update Kecepatan
-        particle.velocity[i] = new_velocity  # Nilainya disetor ke atribut kecepatan suatu partikel
+        new_velocity = w * old_velocity + cognitive + social
+        particle.velocity[i] = new_velocity
 
 def update_position(particle):
-    # Fungsi untuk mengupdate posisi partikel.
 
     for i in range(len(particle.position)):
         new_position = particle.position[i] + particle.velocity[i]
-        # Pastikan nilai posisi berada dalam rentang [-10, 10]
         particle.position[i] = max(-10, min(10, new_position))
 
 def generate_random_positions(dimensi, jumlah_posisi):
-    # Fungsi untuk menghasilkan bilangan acak dalam rentang [-10, 10]
+
     return [[random.uniform(-10, 10) for _ in range(dimensi)] for _ in range(jumlah_posisi)]
 
 def pso(dimensi, jumlah_partikel, jumlah_iterasi, initial_particles=None):
-    # Algoritma Particle Swarm Optimization (PSO).
     particles = initial_particles or [Particle(dimensi, [0.0] * dimensi) for _ in range(jumlah_partikel)]
-    """Jika initial particle tidak diinisialisasi
-    (None) maka menjalankan list comprehension 
-    (membuat particles dengan nilai [0.0])"""
     gbest = min(particles, key=lambda p: p.pbest_value).pbest[:]
-    """gbest diambil dari nilai terkecil menggunakan fungsi (min) 
-    dari atribut pbest_value terkecil dari tiap-tiap partikel
-    lalu salin pbest[:]-nya tersebut menjadi nilai gbest"""
 
     table = PrettyTable()
     table.field_names = ["Iterasi", "Posisi (x,y)", "f(x,y)", "pBest", "gBest", "v"]
@@ -54,27 +42,21 @@ def pso(dimensi, jumlah_partikel, jumlah_iterasi, initial_particles=None):
     iteration_list = []
     position_list = []
 
-    for iteration in range(jumlah_iterasi):  # Melakukan iterasi
+    for iteration in range(jumlah_iterasi):
         iteration_list.append(iteration + 1)
         current_positions = [(particle.position[0], particle.position[1]) for particle in particles]
         position_list.append(current_positions)
         fx_values = [objective_function(particle.position) for particle in particles]
 
-        w = 0.9 - (iteration / jumlah_iterasi) * 0.4  # Menurunkan inertia weight tiap iterasi
+        w = 0.9 - (iteration / jumlah_iterasi) * 0.4
 
-        for particle in particles:  # Tiap partikel juga akan di cek nilainya terkait fungsi objektif
+        for particle in particles:
             current_fitness = objective_function(particle.position)
 
-            """Jika fitness sekarang lebih kecil dari nilai pbest,
-            maka pbest dari partikel adalah posisi partikel sekarang 
-            dan valuenya adalah fitness terkini"""
             if current_fitness < particle.pbest_value:
                 particle.pbest = particle.position[:]
                 particle.pbest_value = current_fitness
 
-            """Kemudian jika current fitness juga lebih kecil
-            dari pada nilai objektif function dari gbest
-            maka si gbest nya diubah menjadi posisi partikel itu"""
             if current_fitness < objective_function(gbest):
                 gbest = particle.position[:]
 
@@ -88,8 +70,7 @@ def pso(dimensi, jumlah_partikel, jumlah_iterasi, initial_particles=None):
                [round(fx, 4) for fx in fx_values],
                [(round(pbest[0], 4), round(pbest[1], 4)) for pbest in [particle.pbest for particle in particles]],
                (round(gbest[0], 3), round(gbest[1], 3)),
-               [(round(velocity[0], 5), round(velocity[1], 5)) for velocity in
-                [particle.velocity for particle in particles]]
+               [(round(velocity[0], 5), round(velocity[1], 5)) for velocity in [particle.velocity for particle in particles]]
                ]
 
         table.add_row(row)
@@ -97,9 +78,6 @@ def pso(dimensi, jumlah_partikel, jumlah_iterasi, initial_particles=None):
         for particle in particles:
             update_position(particle)
 
-    """Jadi akhir dari fungsi ini akan memberikan kita
-    gbest(posisi) dan juga nilainya jika dimasukkan kedalam 
-    objective function"""
     print(table)
 
     # Plot objective function
@@ -126,14 +104,15 @@ def pso(dimensi, jumlah_partikel, jumlah_iterasi, initial_particles=None):
 
     plt.suptitle('Objective Function and Particle Positions')
     plt.show()
+
     return rounded_gbest, round(objective_function(gbest), 4)
 
 if __name__ == "__main__":
     dimensi = 2
     jumlah_partikel = 3
-    jumlah_iterasi = 100
+    jumlah_iterasi = 300
 
-    initial_positions = generate_random_positions(dimensi, 3)  # Generate 3 posisi acak dalam dimensi 2
+    initial_positions = generate_random_positions(dimensi, 3)
     particles = [Particle(dimensi, initial_position) for initial_position in initial_positions]
 
     hasil, nilai_optimum = pso(dimensi, jumlah_partikel, jumlah_iterasi, initial_particles=particles)
